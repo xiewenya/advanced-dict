5edacb44d79aff3e3871ea5b2e1a5af3fc28f3fb
GitHub Action <action@github.com>
2020-10-02 08:07:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/xiewenya/advanceddict/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xiewenya/advanceddict/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xiewenya.advanceddict.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xiewenya.advanceddict.MyBundle
 
 class MyApplicationService {
 
